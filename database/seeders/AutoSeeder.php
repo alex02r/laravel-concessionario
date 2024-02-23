5b6de3234386f62c298f378dc5c183dd8615bf72
asafcastigliano <asafcastigliano@gmail.com>
2024-02-23 15:51:12 +0100

    creato seeder

--- database/seeders/AutoSeeder.php
+++ database/seeders/AutoSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Auto;
 
 class AutoSeeder extends Seeder
 {
@@ -14,6 +16,22 @@ class AutoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 10; $i++){
+            Auto::create([
+                'brand' => $faker->company,
+                'model' => $faker->word,
+                'year' => $faker->year,
+                'type' => $faker->randomElement(['SUV', 'Sedan', 'Hatchback', 'Coupé', 'Convertible']),
+                'fuel_type' => $faker->randomElement(['Petrol', 'Diesel', 'Electric', 'Hybrid']),
+                'displacement' => $faker->numberBetween(1000, 4000),
+                'horsepower' => $faker->numberBetween(50, 400),
+                'description' => $faker->paragraph,
+                'img' => $faker->imageUrl(640, 480, 'cars', true),
+                'price' => $faker->randomNumber(5) . ' €',
+            ]);
+        }
+
     }
 }
