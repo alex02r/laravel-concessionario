4d59e5ee961f45aff2215c24cbf413fa63693f3b
alex02r <alex.giochi02@gmail.com>
2024-02-23 16:42:12 +0100

    [UPGRADE] Creazione form per la modifica di un auto

--- resources/views/admin/autos/edit-auto.blade.php
+++ resources/views/admin/autos/edit-auto.blade.php
@@ -3,7 +3,11 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-8 shadow p-3">
-                    
+                    <form action="{{ route('admin.autos.update', ['auto'=>$auto]) }}" method="post">
+                        @csrf
+                        @method('PUT')
+                        
+                    </form>
                 </div>
             </div>
         </div>
