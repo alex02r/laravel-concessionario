57e3ed3b714a0a60600cf2e6fa56aca04efa30e8
alex02r <alex.giochi02@gmail.com>
2024-02-23 16:36:17 +0100

    [UPGRADE] Creazione file edit auto

---
+++ resources/views/admin/autos/edit-auto.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+    @section('content')
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-8 shadow p-3">
+                    
+                </div>
+            </div>
+        </div>
+    @endsection
