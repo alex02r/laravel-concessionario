bf9512b5852e91a04a174527f67cc60720dc89be
alex02r <alex.giochi02@gmail.com>
2024-03-06 12:24:21 +0100

    [UPGRADE] seeder Optional

--- database/seeders/OptionalSeeder.php
+++ database/seeders/OptionalSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Optional;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class OptionalSeeder extends Seeder
 {
@@ -12,8 +15,22 @@ class OptionalSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $optionals = [
+            'aria condizionata',
+            'sedili riscoldati',
+            'sensore piogga',
+            'sensore parcheggi',
+            'cruise controll'
+        ];
+        foreach ($optionals as $optional_name) {
+            $new_optional = new Optional();
+            $new_optional->name = $optional_name;
+            $new_optional->description = $faker->paragraph();
+            $new_optional->slug = Str::slug($new_optional->name, '-');
+            $new_optional->price = $faker->randomNumber(3, true).'€';
+            $new_optional->save();
+        }
     }
 }
